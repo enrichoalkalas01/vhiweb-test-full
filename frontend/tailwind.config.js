05aea88174582e632d58e9f9e49bb3b6745f5bba
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-10-07 11:05:36 +0700

    add: first fe

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind"
+const withMT = require("@material-tailwind/react/utils/withMT")
+
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        flowbite.content(),
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        flowbite.plugin(),
+    ],
 };
